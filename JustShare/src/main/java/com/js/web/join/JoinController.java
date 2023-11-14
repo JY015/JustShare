401db1a3f76fcd0222174e2d5e9e5ab7ba17511b
JY015 <jaeyun115@naver.com>
2023-11-14 21:58:28 +0900

    11 14 송재윤 회원가입, 문자인증, 정규식

--- JustShare/src/main/java/com/js/web/join/JoinController.java
+++ JustShare/src/main/java/com/js/web/join/JoinController.java
@@ -3,6 +3,7 @@ package com.js.web.join;
 
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,10 +25,11 @@ public class JoinController {
 		}
 	
 	@PostMapping("/join")
-	public String join(@RequestParam Map<String, Object> map, @RequestParam("maddr1") String subAddress) {
-		 String combinedAddress =(String) map.get("maddr")+','+subAddress;
+	public String join(@RequestParam Map<String, Object> map) {
+		System.out.println(map); 
+		String combinedAddress =(String) map.get("maddr") + map.get("maddr2"); 
 		
-		 map.put("maddr2", combinedAddress);
+		 map.put("maddr", combinedAddress);
 		 System.out.println(map);
 		int result = joinService.join(map);
 		
@@ -41,39 +43,17 @@ public class JoinController {
 	}
 
 	
-	@ResponseBody
-	@PostMapping("/emailcheck")
-	public String emailchk(@RequestParam("email") String email ) {
-		/* JSONObject json = new JSONObject(); */
-		System.out.println(email);
-		int result = joinService.emailchk(email);
-		System.out.println(result);
-		
-		return result + "";
-	}
-	
-	
 	//아이디 중복검사
 	@ResponseBody
 	@PostMapping("/checkID")
-	public String checkID(@RequestParam("id") String id) {
-		//System.out.println("id : " + mid);
-		int result = joinService.checkID(id);
-			return result + "";
+	public String checkID(@RequestParam("mid") String mid) {
+		System.out.println("id : " + mid);
+		int result = joinService.checkID(mid);
+		JSONObject json = new JSONObject();
+		json.put("result", result);
+		return json.toString();
 	}
 
-	
-	@ResponseBody
-	@PostMapping("/phonecheck")
-	public String phonechk(@RequestParam("user_phone") int user_phone ) {
-		/* JSONObject json = new JSONObject(); */
-		System.out.println(user_phone);
-		int result = joinService.phonechk(user_phone);
-		System.out.println(result);
-		
-		return result + "";
-	}
-	
 	  
 	// coolSMS 테스트 화면
 	@GetMapping("/sms")
@@ -83,8 +63,8 @@ public class JoinController {
 	    
 	// coolSMS 구현 로직 연결  
 	@GetMapping("/checkSms")
-	public @ResponseBody String sendSMS(@RequestParam(value="phone") String phone) throws CoolsmsException {  	
-		return joinService.PhoneNumberCheck(phone);
+	public @ResponseBody String sendSMS(@RequestParam(value="mphone") String mphone) throws CoolsmsException {  	
+		return joinService.PhoneNumberCheck(mphone);
 	}
 
 }
